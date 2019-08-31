b60a32065e02b1b62d7495b057f272c5842f357b
davidmltz <davidmltz@gmail.com>
2019-08-30 17:36:15 -0700

    Update

--- CompleteUnicodeInPythonCourse/unicode_in_python_1.py
+++ CompleteUnicodeInPythonCourse/unicode_in_python_1.py
@@ -9,5 +9,17 @@
 # 
 
 # 
+# Using VIM to show unicode characters
+#   Enter insert mode ctrl + "i"
+#   Enter ctrl + "v", release and press lowercase "u"
+#     (for character whose code point does not fit in
+#      16 bits, use uppercase "U")
+#   Enter the character's numerical (hexadecimal) unicode
+#     value (code point)
+#   To find numerical (hex) value of unicode character,
+#     place cursor over character and press "GA"
+# 
+
+# 
 # Unicode input and output on the Windows console
 # 
