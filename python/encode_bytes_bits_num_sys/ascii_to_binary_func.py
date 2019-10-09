c6f2bec3b9ee70173845ebb18224ea695e97ab73
davidmltz <davidmltz@gmail.com>
2019-10-09 09:43:13 -0700

    Update notes on Unicode, Python encode/decode

--- python/encode_bytes_bits_num_sys/ascii_to_binary_func.py
+++ python/encode_bytes_bits_num_sys/ascii_to_binary_func.py
@@ -4,6 +4,8 @@
 #
 # ######################################################################
 
+nl = '\n'
+
 
 def brk_ln():
     print("")
@@ -16,13 +18,15 @@ def make_bitseq(s: str) -> str:
 
 
 x_1 = make_bitseq("bits")
-print(x_1)
+print(f"x_1 = 'bits'{nl}Binary Representaion: {x_1}{nl}")
 
 x_2 = make_bitseq("CAPS")
-print(x_2)
+print(f"x_2 = 'CAPS'{nl}Binary Representaion: {x_2}{nl}")
 
 x_3 = make_bitseq("$25.43")
-print(x_3)
+print(f"x_3 = $25.43{nl}Binary Representaion: {x_3}{nl}")
 
 x_4 = make_bitseq("~5")
-print(x_4)
+print(f"x_4 = ~5{nl}Binary Representaion: {x_4}{nl}")
+
+# x_5 = make_bitseq("α")
--- python/encode_bytes_bits_num_sys/python_encoding_notes_examples.py
+++ python/encode_bytes_bits_num_sys/python_encoding_notes_examples.py
@@ -17,7 +17,38 @@
 #
 #
 
+
+# Python string Module - ASCII Character Set
+# string Module Core
+# ######################################################################
+# From lib/python3.7/string.py
+
+whitespace = ' \t\n\r\v\f'
+ascii_lowercase = 'abcdefghijklmnopqrstuvwxyz'
+ascii_uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+ascii_letters = ascii_lowercase + ascii_uppercase
+digits = '0123456789'
+hexdigits = digits + 'abcdef' + 'ABCDEF'
+octdigits = '01234567'
+punctuation = r"""!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"""
+printable = digits + ascii_letters + punctuation + whitespace
+
+# the constants can be used for everyday string manipulation
 # Example:
+import string
+
+s = "What's wrong with ASCII?!?!?!?!?"
+print(s.rstrip(string.punctuation))
+print(type(s))
+
+print(string.printable)
+print(string.punctuation)
+print(str.isprintable(punctuation))
+print(str.isprintable(whitespace))
+
+# ######################################################################
+
+# Stopped Pg 5 10/09/2019
 
 
 a = b"r\xc3\xa9sum\xc3\xa9" .decode("utf-8")
@@ -25,7 +56,17 @@ print(a)
 
 b = None
 
+
+# Get OS native codec
+# ######################################################################
 import locale
 
 y = locale.getpreferredencoding()
 print(y)
+# ######################################################################
+
+# ######################################################################
+# Scratchpad below here
+# ######################################################################
+# print(punctuation)
+# print(printable)
