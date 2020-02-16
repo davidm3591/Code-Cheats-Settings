fc82c2a430edcba41a13a50f016d1cb045f8f6bf
davidmltz <davidmltz@gmail.com>
2020-02-16 08:52:50 -0700

    Zen of Python

---
+++ python/python_zen.py
@@ -0,0 +1,53 @@
+# Python Crash Course, pp 34 - , Zen of Python, Tim Peters
+
+separate = 5 * "----------------"
+print(separate)
+zen_key_points = '''
+Zen of Python, Tim Peters (Key Concepts Only)
+     1. Beautiful is better than ugly.
+        Embrace the notion that code can be beautiful and elegent.
+
+     3. Simple is better than complex.
+        When there is a choice between two working solutions, choose
+        the simple solution instead of the complex solution.
+
+     4. Complex is better than complicated.
+        When a simple solution is unattainable, choose the simplest
+        of the complex solutions.
+
+     7. Readability counts
+        Even when code is complex, aim to make readable by writing
+        informative comments.
+
+    12. There should be one-- and preferably only one --obvious way to do it.
+        Nuts and bolts of your code should be readable to other
+        programmers.
+
+    15. Now is better than never.
+        Do not try to write perfect code. Write code that works
+        and then decide whether to improve your code or not.
+'''
+zen_all = '''
+Zen of Python, Tim Peters (Complete)
+     1. Beautiful is better than ugly.
+     2. Explicit is better than implicit.
+     3. Simple is better than complex.
+     4. Complex is better than complicated.
+     5. Flat is better than nested.
+     6. Sparse is better than dense.
+     7. Readability counts.
+     8. Special cases aren't special enough to break the rules.
+     9. Although practicality beats purity.
+    10. Errors should never pass silently.
+    11. Unless explicitly silenced.
+    12. In the face of ambiguity, refuse the temptation to guess.
+    13. There should be one-- and preferably only one --obvious way to do it.
+    14. Although that way may not be obvious at first unless you're Dutch.
+    15. Now is better than never.
+    16. Although never is often better than *right* now.
+    17. If the implementation is hard to explain, it's a bad idea.
+    18. If the implementation is easy to explain, it may be a good idea.
+    19. Namespaces are one honking great idea -- let's do more of those!
+'''
+print(zen_key_points)
+print(zen_all)
