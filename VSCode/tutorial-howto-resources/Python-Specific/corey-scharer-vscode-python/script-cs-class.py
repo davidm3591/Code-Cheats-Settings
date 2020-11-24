3b14575cf4eaaef91c662818c5334582a874bf14
davidmltz <davidmltz@gmail.com>
2020-11-23 20:41:17 -0700

    Add files for Corey Schafer's Visual Studio Code for Python class

---
+++ VSCode/tutorial-howto-resources/Python-Specific/corey-scharer-vscode-python/script-cs-class.py
@@ -0,0 +1,21 @@
+# Corey Schafer Python for Visual Studio Code
+# Available: https://www.youtube.com/watch?v=-nh9rCzPJ20
+# Date of video tutorial: 5/1/2020
+
+import sys
+
+print(sys.version)
+print(sys.executable)
+
+def greet(who_to_greet):
+    """Function to greet me."""
+    greeting = 'Hello, {}'.format(who_to_greet)
+    return greeting
+
+print(greet('World'))
+print(greet('David'))
+# ======================================
+# Stopped video at 23:26
+# ======================================
+print("")
+print("Stopped video at 23:26")
