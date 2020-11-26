af41f91ab79b660b96ae1985ac387b1e6ccd339f
davidmltz <davidmltz@gmail.com>
2020-11-25 21:13:27 -0700

    Add content for VSCode for python youtube tutorial

--- VSCode/tutorial-howto-resources/Python-Specific/corey-scharer-vscode-python/script-cs-class.py
+++ VSCode/tutorial-howto-resources/Python-Specific/corey-scharer-vscode-python/script-cs-class.py
@@ -26,14 +26,18 @@ def print_stuff(stuff_to_print):
     """Print whatever I hand off to it"""
     print(stuff_to_print)
 
-heading = "Class Notes\n\tTopic and Time"
+heading = "Class Notes\nTopic and Time"
 sep_line = "------------------------------------------------------------------"
 
-vscode_venv = "\tCreating  a Virtual Environment '('venv')' at 26:12"
+vscode_venv = "Creating  a Virtual Environment (venv) at 26:12"
+code_formatter = "Installing PEP 8, or Black to format code at 32:17"
 
+
+print_stuff(sep_line)
 print_stuff(heading)
 print_stuff(sep_line)
 
 print_stuff(vscode_venv)
+print_stuff(code_formatter)
 
-print_stuff(sep_line)
+print('')
