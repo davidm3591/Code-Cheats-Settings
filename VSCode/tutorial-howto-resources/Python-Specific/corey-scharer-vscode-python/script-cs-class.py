c1d33daa9b8d78317f949ea0f9b06142f866bd9b
davidmltz <davidmltz@gmail.com>
2020-11-27 17:13:09 -0700

    Update notes

--- VSCode/tutorial-howto-resources/Python-Specific/corey-scharer-vscode-python/script-cs-class.py
+++ VSCode/tutorial-howto-resources/Python-Specific/corey-scharer-vscode-python/script-cs-class.py
@@ -6,7 +6,7 @@ import sys
 
 print(sys.version)
 print(sys.executable)
-
+n = 0
 def greet(who_to_greet):
     """Function to greet me."""
     greeting = 'Hello, {}'.format(who_to_greet)
@@ -22,9 +22,16 @@ print("")
 print("Stopped video at 23:26")
 # ======================================
 
+
 def print_stuff(stuff_to_print):
     """Print whatever I hand off to it"""
     print(stuff_to_print)
+
+n = 0
+def print_notes(notes_to_print):
+    """Print whatever notes I hand off"""
+    # n += 1
+    print(notes_to_print)
 
 heading = "Class Notes\nTopic and Time"
 sep_line = "------------------------------------------------------------------"
@@ -32,13 +39,17 @@ sep_line = "------------------------------------------------------------------"
 vscode_venv = "Creating  a Virtual Environment (venv) at 26:12"
 code_formatter = "Installing PEP 8, or Black to format code at 32:17"
 git_setup = "Setting up and using GIT in VS Code at "
+# vscode_venv = f"{n}. Creating  a Virtual Environment (venv) at 26:12"
+# code_formatter = f"{n}. Installing PEP 8, or Black to format code at 32:17"
+# git_setup = f"{n}. Setting up and using GIT in VS Code at "
 
 
 print_stuff(sep_line)
 print_stuff(heading)
 print_stuff(sep_line)
 
-print_stuff(vscode_venv)
-print_stuff(code_formatter)
+print_notes(vscode_venv)
+print_notes(code_formatter)
+print_notes(git_setup)
 
 print('')
