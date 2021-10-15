a48521c0270efb058d3c191bbb2e92de9accd97b
davidmltz <davidmltz@gmail.com>
2021-10-15 16:01:34 -0700

    Setup for Corey Schafer Youtube Python3 regex tut

---
+++ regex_stuff/coreyMS_python3_regex/corey_s_python3_regex.py
@@ -0,0 +1,30 @@
+import re # standard Python regular expression module
+
+# 
+# text to search from: beginning_text_for_regex_cs.txt
+# 
+
+text_to_search = '''
+abcdefghijklmnopqurtuvwxyz
+ABCDEFGHIJKLMNOPQRSTUVWXYZ
+1234567890
+
+Ha HaHa
+
+MetaCharacters (Need to be escaped):
+. ^ $ * + ? { } [ ] \ | ( )
+
+coreyms.com
+
+321-555-4321
+123.555.1234
+123*555*1234
+800-555-1234
+900-555-1234
+
+Mr. Schafer
+Mr Smith
+Ms Davis
+Mrs. Robinson
+Mr. T
+'''
